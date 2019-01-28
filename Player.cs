198aac7fa64acb3b124729f36b0a7702a7f3c3e5
Desktop Hero <43973935+DanBomb@users.noreply.github.com>
2019-01-28 09:33:31 -0500

    Added files

---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    int handMaxSize = 5;
+    public List<Card> Hand = new List<Card>();
+
+    
+}
