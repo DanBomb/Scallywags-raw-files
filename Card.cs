f72ef96453362221d96a153005c0d0daa17a0269
Desktop Hero <43973935+DanBomb@users.noreply.github.com>
2019-01-28 09:48:32 -0500

    New files

---
+++ Card.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card
+{
+    public string name = "Card1";
+    public string desc = "This is a card";
+
+}
