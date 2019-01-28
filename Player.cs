282208e88e5d1b3586ae58bcba7ef0908a893e2d
Desktop Hero <43973935+DanBomb@users.noreply.github.com>
2019-01-28 10:17:15 -0500

    Update

--- Player.cs
+++ Player.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 
 public class Player
 {
     int handMaxSize = 5;
-    public Card 
     public List<Card> Hand = new List<Card>();
 
     public void DrawCard () {
-        Card card1 = new Card;
+        Card card1 = new Card();
         card1.name = "Cameron";
         card1.desc = "A card named Cameron";
         Hand.Add(card1);
+        Console.Log(Hand);
     }
+
+    DrawCard();
 }
---
+++ testingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
